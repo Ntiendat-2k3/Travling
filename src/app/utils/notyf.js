f0b54176010d31735d03376e1e10186e30796752
NTiendat <nguyentiendatg2003@gmail.com>
2025-03-07 00:49:00 +0700

    fix notyf

--- src/app/utils/notyf.js
+++ src/app/utils/notyf.js
@@ -1,23 +1,22 @@
-import { useEffect } from "react";
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 
-// Hàm để tạo Notyf
-export const useNotyf = () => {
-  useEffect(() => {
-    // Tạo instance của Notyf
-    const notyf = new Notyf({
-      duration: 2000,
-      position: { x: "center", y: "bottom" },
-      ripple: true, // Hiệu ứng ripple
-    });
+const useNotyf = () => {
+  const notyf = new Notyf({
+    duration: 3000,
+    position: { x: "center", y: "bottom" },
+    dismissible: true,
+  });
 
-    // Return các hàm thông báo
-    return {
-      showSuccess: (message) => notyf.success(message), // Thông báo thành công
-      showError: (message) => notyf.error(message), // Thông báo lỗi
-      showInfo: (message) => notyf.info(message), // Thông báo thông tin
-      showWarning: (message) => notyf.warning(message), // Thông báo cảnh báo
-    };
-  }, []); // Chạy 1 lần khi component mount
+  const showSuccess = (message) => {
+    notyf.success(message);
+  };
+
+  const showError = (message) => {
+    notyf.error(message);
+  };
+
+  return { showSuccess, showError };
 };
+
+export { useNotyf };
